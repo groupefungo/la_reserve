4ea8f701beffd766ae6593ceaa369c5b0f4d70f3
Rémi Gagnon <rem.gagnon@gmail.com>
2017-01-11 13:16:16 -0500

    unicorn, capistrano et deploy stuffs

---
+++ config/deploy/staging.rb
@@ -0,0 +1,49 @@
+set :stage, :staging
+
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+role :app, %w{petrostclaire.madedev.com}
+role :web, %w{petrostclaire.madedev.com}
+role :db,  %w{petrostclaire.madedev.com}
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+server 'petrostclaire.madedev.com', user: 'web', roles: %w{web app}, my_property: :my_value
+
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+# and/or per server
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
+# setting per server overrides global ssh_options
+
+# fetch(:default_env).merge!(rails_env: :production)
+
+set :rvm_ruby_version, '2.2.3@lareserve'
+
+set :rails_env, :staging
+
+set :deploy_to, '/home/web/apps/staging/la_reserve_ste_claire'
+set :linked_files, %w{config/database.yml config/unicorn/staging.rb .env}
