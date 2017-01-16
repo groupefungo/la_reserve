12ddc071918fce1f6b05e08a23d6ea8538e58516
Dominic Michaud <dominic@ateliermade.ca>
2017-01-16 13:06:58 -0500

    add arrows for mobile promos slider homepage

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,3 +59,14 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
+
+task :seed do
+  puts "\n=== Seeding Database ===\n"
+  on primary :db do
+    within current_path do
+      with rails_env: fetch(:stage) do
+        execute :rake, 'db:seed'
+      end
+    end
+  end
+end
