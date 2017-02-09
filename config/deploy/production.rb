9a28b95716ca7bb299ea600a043864ac50db8a32
Rémi Gagnon <rem.gagnon@gmail.com>
2017-02-09 11:55:47 -0500

    unicorn, capistrano et deploy stuffs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,54 +1,32 @@
-# server-based syntax
+set :stage, :production
+
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+role :app, %w{petrostclaire.madedev.com}
+role :web, %w{petrostclaire.madedev.com}
+role :db,  %w{petrostclaire.madedev.com}
+
+# Extended Server Syntax
 # ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+server 'petrostclaire.madedev.com', user: 'web', roles: %w{web app}, my_property: :my_value
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
-# server 'db.example.com', user: 'deploy', roles: %w{db}
-
-
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any  hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
 #  set :ssh_options, {
 #    keys: %w(/home/rlisowski/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
+# and/or per server
 # server 'example.com',
 #   user: 'user_name',
 #   roles: %w{web app},
@@ -59,3 +37,13 @@
 #     auth_methods: %w(publickey password)
 #     # password: 'please use keys'
 #   }
+# setting per server overrides global ssh_options
+
+# fetch(:default_env).merge!(rails_env: :production)
+
+set :rvm_ruby_version, '2.2.3@lareserve'
+
+set :rails_env, :production
+
+set :deploy_to, '/home/web/apps/la_reserve_ste_claire'
+set :linked_files, %w{config/database.yml config/unicorn/production.rb .env}
