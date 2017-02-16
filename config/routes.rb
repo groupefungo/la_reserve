d98986257158530b152ce7f48ce09ab80ee7b602
Dominic Michaud <dominic@ateliermade.ca>
2017-02-16 16:08:49 -0500

    Permettre l'ajout d'utilisateur, seulement si le master admin est signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do
-    get "users/sign_up", :to => "devise/sessions#new", :as => :sign_up
-    get 'users/edit' => 'devise/sessions#new', :as => 'edit_user_registration'
-  end
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   resources :contacts
